84a8c99b513495b3c3ef038aec5dafca336eead6
qmayker <qmayker9@gmail.com>
2026-04-20 18:07:48 +0300

    Added user profile model

--- todo/accounts/models.py
+++ todo/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile"
+    )
+    timezone = models.CharField(default='Europe/Kyiv')
+
+    def __str__(self):
+        return f"{self.user}'s profile"
