087d48b29fa47d485e07e3fed8e697fdc8b7b282
qmayker <qmayker9@gmail.com>
2026-05-07 17:52:38 +0300

    fixed problems(None as return in get_task_keys and import error)

--- todo/tasks/services/redis_keys.py
+++ todo/tasks/services/redis_keys.py
@@ -1,16 +1,15 @@
-from django.contrib.contenttypes.models import ContentType
-
-
 def get_task_keys(id: int, ct_id: int, end: bool = False, delete: bool = False) -> dict:
     keys = {"key": f"task:{id}:{ct_id}"}
     if not delete:
         keys.update({"lock_key": f"{id}:{ct_id}:{end}"})
     else:
         keys.update({"lock_key": f"delete:{id}:{ct_id}"})
-    return
+    return keys
 
 
 def get_delete_by_object(object):
+    from django.contrib.contenttypes.models import ContentType
+
     ct = ContentType.objects.get_for_model(object)
     ct_id = ct.id
     id = object.id
