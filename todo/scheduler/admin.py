b8b91349100bf70a65d6ae83dfb2930998a971d8
qmayker <qmayker9@gmail.com>
2026-05-08 19:01:41 +0300

    added scheduler.task to admin

--- todo/scheduler/admin.py
+++ todo/scheduler/admin.py
@@ -5,7 +5,9 @@ from .models import Task
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['celery_id', 'task_name']
+    list_display = ['celery_id', 'task_name', 'start']
+    list_filter = ['start']
+    search_fields = ['task_name']
 
 
     def task_name(self, obj:Task):
