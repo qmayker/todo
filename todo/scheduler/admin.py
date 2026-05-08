578434bef1efac7f52c22374cdaf4310c8733ff1
qmayker <qmayker9@gmail.com>
2026-05-08 18:57:49 +0300

    added missing migration

--- todo/scheduler/admin.py
+++ todo/scheduler/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['celery_id', 'task_name']
+
+
+    def task_name(self, obj:Task):
+        return obj.task.name
